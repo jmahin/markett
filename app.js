e8509af02d2102af924f2d96b12b1c8d8a66225a
jmahin <mahinjo@icloud.com>
2022-05-25 18:55:18 -0400

    update

--- app.js
+++ app.js
@@ -1,26 +1,38 @@
+
 const express = require('express');
+const morgan = require('morgan');
+// const methodOverride = require('method-override');
+const path = require('path');
 const app = express();
 const PORT = 3000;
-const morgan = require('morgan');
-app.use(morgan('combined'));
 
-const path = require('path');
+app.use(morgan('combined'));
 app.use(express.static(path.join(__dirname, 'public')));
+// method override
+// app.use(methodOverride('_method'));
 
 app.set('view engine', 'ejs');
 
+app.listen(PORT, () => {
+    console.log(`The local host URL is http://localhost:${PORT}`);
+});
+
 app.get('/', (request, response) => {
-    response.render('pages/index'), {
+    response.render('pages/index')
+})
 
-    }
-});
+// app.get('/', (request, response) => {
+//     response.render('pages/index'), {
 
-app.get('/admin', (request, response) => {
-    response.render('pages/admin');
-});
-app.get('/contact', (request, response) => {
-    response.render('pages/contact');
-});
-app.get('/login', (request, response) => {
-    response.render('pages/login');
-});
+//     }
+// });
+
+// app.get('/admin', (request, response) => {
+//     response.render('pages/admin');
+// });
+// app.get('/contact', (request, response) => {
+//     response.render('pages/contact');
+// });
+// app.get('/login', (request, response) => {
+//     response.render('pages/login');
+// });
