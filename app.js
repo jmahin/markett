7d6f550e3f07e1652303c04c97ce8113c5c31a6e
jmahin <mahinjo@icloud.com>
2022-05-26 02:02:16 -0400

    update

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 
 const express = require('express');
 const morgan = require('morgan');
-// const methodOverride = require('method-override');
+const methodOverride = require('method-override');
 const path = require('path');
 const app = express();
 const PORT = 3000;
 
 app.use(morgan('combined'));
 app.use(express.static(path.join(__dirname, 'public')));
-// app.use(methodOverride('_method'));
+app.use(methodOverride('_method'));
 
 app.set('view engine', 'ejs');
 
