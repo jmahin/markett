263267c98befb171098bc8285c87a76390f2a805
jmahin <mahinjo@icloud.com>
2022-06-04 10:21:28 -0400

    update

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose');
+const dotenv = require("dotenv");
 
-mongoose.connect(process.env.DB_URL, {useNewUrlParser: true, useUnifiedTopology: true}, (err) => {
-  if(!err) {
-    console.log("Successful connection with MongoDB Server");  
+mongoose.connect(process.env.DB_URL, { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
+  if (!err) {
+    console.log("Successful connection with MongoDB Server");
   }
   else {
-      console.log("Error with MongoDB's connectivity");
-      console.log(err)
+    console.log("Error with MongoDB's connectivity");
+    console.log(err)
   }
 });
